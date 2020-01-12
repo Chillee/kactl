4ea0b9315546bbf683552721b8b7b6075ec692ef
Horace He <horacehe2007@yahoo.com>
2020-01-12 05:24:33 -0500

    made some modifications

--- content/graph/LCT.h
+++ content/graph/LCT.h
@@ -1,21 +1,22 @@
 /**
- * Author: 
+ * Author:
  * Description: link-cut Tree. Supports BST-like augmentations. (Can be used in place of HLD).
  * Current implementation supports update value at a node, and query max on a path.
  * For details about the structure, refer to https://en.wikipedia.org/wiki/Link/cut_tree
  * Tested on: http://acm.timus.ru/problem.aspx?num=1553
  * Status: Passes existing fuzz tests (with function names modified).
  */
+int gid = 0;
 struct Node {
 	bool flip = 0;
 	 // pp = path parent, p = splay tree parent
 	Node *pp, *p, *c[2];
 	// add stuff
-	int val = 0, cval = 0;
+	int val = 0, cval = 0, id = gid++;
 	Node() { pp = p = c[0] = c[1] = 0; }
 	void push() {
 		if (flip) {
-			rep(i, 0, 2) if (c[i]) c[i]->flip ^= 1;
+			trav(x, c) if (x) x->flip ^= 1;
 			swap(c[0], c[1]); flip = 0;
 		}
 	}
@@ -53,36 +54,47 @@ struct Node {
 			z->c[1] = y; z->pull();
 		}
 		splay();
-		flip ^= 1;
+		return this;
+	}
+	Node* makeRoot() {
+		access(), flip ^= 1;
 		return this;
 	}
 };
 struct LinkCut {
-	vector<Node> nodes;
-	LinkCut(int N) : nodes(N) {}
-	bool cut(int u, int v) { /// start-hash
-		Node *y = nodes[v].access();
-		Node *x = nodes[u].access();
+	vector<Node> ns;
+	LinkCut(int N) : ns(N) {}
+	bool cut(int u) {
+		Node *y = ns[v].makeRoot();
+		Node *x = ns[u].access();
 		if (x->c[0] != y || y->c[1]) return false;
 		x->c[0] = y->p = y->pp = 0;
 		x->pull();
 		return true;
-	} /// end-hash
+	}
 	bool isConnected(int u, int v) {
-		Node *x = nodes[u].access();
-		Node *y = nodes[v].access();
-        return x == y || x -> p;
+		Node *x = ns[u].access();
+		Node *y = ns[v].access();
+		return x == y || x -> pp || x->p;
+	}
+	int lca(int u, int v) {
+		Node *x = ns[u].access(), *y = ns[v].access();
+		if (!(x == y || x->pp || x->p)) return -1;
+		x->splay();
+		return (x->pp ? x->pp : x)->id;
 	}
 	bool link(int u, int v) {
-		if (isConnected(u, v)) return false;
-		nodes[u].access()->pp = &nodes[v];
+		if (lca(u, v) != -1) return false;
+		auto t = ns[u].makeRoot();
+		t->pp = &ns[v];
 		return true;
 	}
 	void update(int u, int c) {
-		nodes[u].access()->val += c;
+		auto t = ns[u].access();
+		t->val += c;
 	}
 	int query(int u, int v) { // Find max on the path.
-		nodes[v].access();
-		return nodes[u].access()->cval;
+		ns[v].makeRoot();
+		return ns[u].access()->cval;
 	}
 };
